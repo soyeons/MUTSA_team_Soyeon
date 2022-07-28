2f4223fc24a12d738844cc2e2a00b8ec358e83f4
chaenLIM <limce21@naver.com>
2022-07-28 16:25:48 +0900

    [feat] 프론트엔드 1차 머지 성공!

--- src/App.js
+++ src/App.js
@@ -6,15 +6,38 @@ import Calendar from './Calendar';
 import FestList from './FestList';
 import Login from './Login';
 import Redirect from './Redirect';
-
+import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import CMreview from './pages/CM_review';
+import CMfriends from './pages/CM_friends';
+import CMinf from './pages/CM_inf';
+import CMticket from './pages/CM_ticket';
+import WritePost from './pages/WritePost';
+import RVShowPost from './pages/RVShowPost';
+import FRShowPost from './pages/FRShowPost';
+import TKShowPost from './pages/TKShowPost';
+import InfShowPost from './pages/InfShowPost';
+import { RightWrite } from './pages/right';
 
 function App() {
   return (
     <>
+      {/* <Detail></Detail>
+      <Calendar></Calendar>
+      <FestList></FestList> */}
       <Routes>
         <Route path='/' element={<Login></Login>}></Route>
         <Route path='/redirect' element={<Redirect></Redirect>}></Route>
+        <Route path='/review' element={<CMreview />} />
+        <Route path='/friends' element={<CMfriends />} />
+        <Route path='/ticket' element={<CMticket />} />
+        <Route path='/inf' element={<CMinf />} />
+        <Route path='/writepost' element={<WritePost />} />
+        <Route path='/review/post/:postID' element={<RVShowPost />} />
+        <Route path='/friends/post/:postID' element={<FRShowPost />} />
+        <Route path='/ticket/post/:postID' element={<TKShowPost />} />
+        <Route path='/inf/post/:postID' element={<InfShowPost />} />
+        <Route path='/writepost' component={RightWrite} />
       </Routes>
     </>
   );
