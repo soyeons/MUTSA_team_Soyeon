f5d1a1c3a0b446de14c4b981de22c137d50e4ede
chaenLIM <limce21@naver.com>
2022-07-28 15:27:11 +0900

    [feat] Calendar, Detail, Festlist 페이지 구현

---
+++ src/FestList.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import "./FestList.css";
+
+const initialPostList = [
+  {
+    id: 1,
+    title: "페스티벌1",
+    startDate: "20220727",
+    endDate: "20220728",
+    imgUrl: "http://placekitten.com/300/400",
+    popular: 20,
+  },
+  {
+    id: 2,
+    title: "페스티벌2",
+    startDate: "20220701",
+    endDate: "20220702",
+    imgUrl: "http://placekitten.com/300/400",
+    popular: 15,
+  },
+  {
+    id: 3,
+    title: "페스티벌3",
+    startDate: "20220731",
+    endDate: "20220801",
+    imgUrl: "http://placekitten.com/300/400",
+    popular: 100,
+  },
+  {
+    id: 4,
+    title: "페스티벌4",
+    startDate: "20220802",
+    endDate: "20220803",
+    imgUrl: "http://placekitten.com/300/400",
+    popular: 2,
+  },
+];
+
+const FestList = () => {
+  const [Content, setContent] = useState();
+
+  const onChangeHanlder = (e) => {
+    setContent(e.currentTarget.value);
+    let state = e.currentTarget.value;
+    if (state === "register") {
+      initialPostList.sort((a, b) => {
+        return a.id - b.id;
+      });
+    } else if (state === "popular") {
+      initialPostList.sort((a, b) => {
+        return b.popular - a.popular;
+      });
+    } else if (state === "fast") {
+      initialPostList.sort((a, b) => {
+        let aStartdate = new Date(Number(a.startDate.substring(0, 4)), Number(a.startDate.substring(4, 6)) - 1, Number(a.startDate.substring(6, 8)));
+        let bStartdate = new Date(Number(b.startDate.substring(0, 4)), Number(b.startDate.substring(4, 6)) - 1, Number(b.startDate.substring(6, 8)));
+        return aStartdate - bStartdate;
+      });
+    }
+  };
+  const Options = [
+    { key: "register", value: "등록순" },
+    { key: "popular", value: "인기순" },
+    { key: "fast", value: "빠른 일정순" },
+  ];
+
+  const getList = () => {
+    let festArr = [];
+    let dateList = [];
+    for (let i = 0; i < initialPostList.length; i++) {
+      let start = new Date(Number(initialPostList[i].startDate.substring(0, 4)), Number(initialPostList[i].startDate.substring(4, 6)) - 1, Number(initialPostList[i].startDate.substring(6, 8)));
+      let end = new Date(Number(initialPostList[i].endDate.substring(0, 4)), Number(initialPostList[i].endDate.substring(4, 6)) - 1, Number(initialPostList[i].endDate.substring(6, 8)));
+      let startYear = start.getFullYear();
+      let endYear = end.getFullYear();
+      let startMonth = start.getMonth() + 1;
+      let endMonth = end.getMonth() + 1;
+      let startDate = start.getDate();
+      let endDate = end.getDate();
+
+      dateList.push([startYear, startMonth, startDate, endYear, endMonth, endDate]);
+    }
+    for (let i = 0; i < initialPostList.length; i++) {
+      festArr.push(
+        <div className="listBack">
+          <div className="listContent">
+            <img src={initialPostList[i].imgUrl} alt="poster" />
+            <div className="listDesc">
+              <div className="listTitle">{initialPostList[i].title}</div>
+              <div className="listCal">
+                {dateList[i][0]}.{dateList[i][1]}.{dateList[i][2]} ~ {dateList[i][3]}.{dateList[i][4]}.{dateList[i][5]}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return festArr;
+  };
+
+  return (
+    <div className="listBody">
+      <div className="listHeader">
+        <select onChange={onChangeHanlder} value={Content}>
+          <option>리스트 필터</option>
+          {Options.map((item, index) => (
+            <option key={item.key} value={item.key}>
+              {item.value}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="listMain">{getList()}</div>
+    </div>
+  );
+};
+
+export default FestList;
---
+++ src/PopUp.js
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import style from './PopUp.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(style);
+
+const initialPostList = [
+  {
+    festival_id: 1,
+    startDate: '20220703',
+    endDate: '20220704',
+    title: '페스티벌1',
+    place: '인천 송도 달빛 축제 공원',
+    imgUrl: 'http://placekitten.com/300/400',
+    reservation_date: '20220624',
+    lineUp: ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff']
+  },
+  {
+    festival_id: 2,
+    startDate: '20220710',
+    endDate: '20220710',
+    title: '페스티벌2',
+    place: '인천 송도 달빛 축제 공원',
+    imgUrl: 'http://placekitten.com/300/400',
+    reservation_date: '20220625',
+    lineUp: [
+      'aaa',
+      'bbb',
+      'ccc',
+      'ddd',
+      'eee',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      '00000000',
+      'aaa',
+      'bbb',
+      'ccc',
+      'ddd',
+      'eee',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      'fff',
+      '00000000'
+    ]
+  },
+  {
+    festival_id: 3,
+    startDate: '20220717',
+    endDate: '20220719',
+    title: '페스티벌3',
+    place: '인천 송도 달빛 축제 공원',
+    imgUrl: 'http://placekitten.com/300/400',
+    reservation_date: '20220626',
+    lineUp: ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff']
+  },
+  {
+    festival_id: 4,
+    startDate: '20220724',
+    endDate: '20220724',
+    title: '페스티벌4',
+    place: '인천 송도 달빛 축제 공원',
+    imgUrl: 'http://placekitten.com/300/400',
+    reservation_date: '20220627',
+    lineUp: ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff']
+  },
+  {
+    festival_id: 5,
+    startDate: '20220731',
+    endDate: '20220731',
+    title: '페스티벌5',
+    place: '인천 송도 달빛 축제 공원',
+    imgUrl: 'http://placekitten.com/300/400',
+    reservation_date: '20220628',
+    lineUp: ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff']
+  }
+];
+
+const PopUp = (props) => {
+  const idx = props.idx;
+  const y = props.y;
+  console.log(y);
+  const [postList] = useState(initialPostList);
+  const dayArr = ['일', '월', '화', '수', '목', '금', '토'];
+
+  const startDate = new Date(Number(postList[idx].startDate.substring(0, 4)), Number(postList[idx].startDate.substring(4, 6) - 1), Number(postList[idx].startDate.substring(6, 8)));
+  const endDate = new Date(Number(postList[idx].endDate.substring(0, 4)), Number(postList[idx].endDate.substring(4, 6) - 1), Number(postList[idx].endDate.substring(6, 8)));
+  const reserveDate = new Date(
+    Number(postList[idx].reservation_date.substring(0, 4)),
+    Number(postList[idx].reservation_date.substring(4, 6) - 1),
+    Number(postList[idx].reservation_date.substring(6, 8))
+  );
+
+  let dateString = `${startDate.getFullYear()}.${startDate.getMonth() + 1}.${startDate.getDate()}.${dayArr[startDate.getDay()]} ~ ${endDate.getFullYear()}.${
+    endDate.getMonth() + 1
+  }.${endDate.getDate()}.${dayArr[endDate.getDay()]}`;
+  const subDate = endDate.getDate() - startDate.getDate();
+  if (subDate === 0) {
+    dateString = `${startDate.getFullYear()}.${startDate.getMonth() + 1}.${startDate.getDate()}.${dayArr[startDate.getDay()]}`;
+  }
+
+  const addTags = (lineUp) => {
+    let lineUpArr = [];
+    for (let i = 0; i < lineUp.length; i++) {
+      lineUpArr.push(<li>{lineUp[i]}</li>);
+    }
+    return lineUpArr;
+  };
+
+  return (
+    <div>
+      <div className={cx({ popup: true }, { popupHigh: y < 540 }, { popupLow: y >= 540 })}>
+        <img id='popupPoster' src={postList[idx].imgUrl} alt='' />
+        <div className='popupDesc'>
+          <div className='popupTitleContainer'>
+            <div className='popupTitle'>
+              <p>{postList[idx].title}</p>
+            </div>
+            <div className='popupPerformanceDate'>
+              <p>{dateString}</p>
+            </div>
+            <div className='popupPlace'>
+              <p>{postList[idx].place}</p>
+            </div>
+          </div>
+          <div className='popupLineUp'>
+            <ul>{addTags(postList[idx].lineUp)}</ul>
+          </div>
+          <div className='popupOpenDate'>
+            <p>
+              티켓 오픈 {reserveDate.getFullYear()}.{reserveDate.getMonth() + 1}.{reserveDate.getDate()}.{dayArr[reserveDate.getDay()]}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopUp;
