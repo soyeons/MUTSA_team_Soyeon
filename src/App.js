942003735d4e47a16be17c65886a5ec844b3aeac
chaenLIM <limce21@naver.com>
2022-08-19 22:43:47 +0900

    [fix] 네브바 위치 재수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import userContext from './context';
@@ -23,56 +23,57 @@ import InfShowPost from './pages/InfShowPost';
 import EditPost from './pages/EditPost';
 import Search from './Search';
 
-const API_URL = 'http://172.17.195.227:8000/festivalapp/'
+const API_URL = 'http://172.17.195.227:8000/festivalapp/';
 
 function App() {
-  const [user, setUser] = useState({name: "", email: "", like: "", post: "", reply: ""})
-  const context = useContext(userContext);
+	const [user, setUser] = useState({ name: '', email: '', like: '', post: '', reply: '' });
+	const context = useContext(userContext);
 
-  const initializeUserInfo = async () => {
-    if(localStorage.setItem.logined === true){
-    context.setUser({
-      kakaoId : localStorage.getItem("kakaoId"),
-      name : localStorage.getItem("name"),
-      email : localStorage.getItem("email"),
-      post : localStorage.getItem("post"),
-      reply : localStorage.getItem("reply"),
-      logined : localStorage.getItem("logined"),
-    })}
-  }
+	const initializeUserInfo = async () => {
+		if (localStorage.setItem.logined === true) {
+			context.setUser({
+				kakaoId: localStorage.getItem('kakaoId'),
+				name: localStorage.getItem('name'),
+				email: localStorage.getItem('email'),
+				post: localStorage.getItem('post'),
+				reply: localStorage.getItem('reply'),
+				logined: localStorage.getItem('logined')
+			});
+		}
+	};
 
-  useEffect(()=> {
-    initializeUserInfo();
-  })
+	useEffect(() => {
+		initializeUserInfo();
+	});
 
-  return (
-    <>
-      <userContext.Provider value = {{user,setUser}}>
-        <Routes>
-          <Route path='/' element={<Main/>}/>
-          <Route path='/calendar' element={<Calendar/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path = '/redirect' element = {<Redirect></Redirect>}/>
-          <Route path = '/mypage' element={<Profile></Profile>}></Route> 
-          <Route path="/festlist" element={<FestList/>}/>
-          {/* <Route path="/festlist/checking" element={<FestCheck/>}/> */}
-          <Route path='/detail/:id' element={<Detail/>}/>
-          <Route path='/review' element={<CMreview apiUrl={API_URL}/>} />
-          <Route path='/friends' element={<CMfriends apiUrl={API_URL}/>} />
-          <Route path='/ticket' element={<CMticket apiUrl={API_URL}/>} />
-          <Route path='/inf' element={<CMinf apiUrl={API_URL}/>} />
-          <Route path='/writepost' element={<WritePost apiUrl={API_URL}/>} exact/>
-          <Route path='/writepost/modify/:postID' element={<WritePost apiUrl={API_URL}/>} exact/>          
-          <Route path='/review/post/:postID' element={<RVShowPost apiUrl={API_URL}/>} />
-          <Route path='/friends/post/:postID' element={<FRShowPost apiUrl={API_URL}/>} />
-          <Route path='/ticket/post/:postID' element={<TKShowPost apiUrl={API_URL}/>} />
-          <Route path='/inf/post/:postID' element={<InfShowPost apiUrl={API_URL}/>} />
-          <Route path='/writepost' element={<WritePost apiUrl={API_URL}/>} />
-        </Routes>
-      </userContext.Provider>
-    </>
-  );
+	return (
+		<>
+			<userContext.Provider value={{ user, setUser }}>
+				<Routes>
+					<Route path='/' element={<Main />} />
+					<Route path='/calendar' element={<Calendar />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/redirect' element={<Redirect></Redirect>} />
+					<Route path='/mypage' element={<Profile></Profile>}></Route>
+					<Route path='/festlist' element={<FestList />} />
+					{/* <Route path="/festlist/checking" element={<FestCheck/>}/> */}
+					<Route path='/detail/:id' element={<Detail />} />
+					<Route path='/review' element={<CMreview apiUrl={API_URL} />} />
+					<Route path='/friends' element={<CMfriends apiUrl={API_URL} />} />
+					<Route path='/ticket' element={<CMticket apiUrl={API_URL} />} />
+					<Route path='/inf' element={<CMinf apiUrl={API_URL} />} />
+					<Route path='/writepost' element={<WritePost apiUrl={API_URL} />} exact />
+					<Route path='/writepost/modify/:postID' element={<WritePost apiUrl={API_URL} />} exact />
+					<Route path='/review/post/:postID' element={<RVShowPost apiUrl={API_URL} />} />
+					<Route path='/friends/post/:postID' element={<FRShowPost apiUrl={API_URL} />} />
+					<Route path='/ticket/post/:postID' element={<TKShowPost apiUrl={API_URL} />} />
+					<Route path='/inf/post/:postID' element={<InfShowPost apiUrl={API_URL} />} />
+					<Route path='/writepost' element={<WritePost apiUrl={API_URL} />} />
+					<Route path='/search' element={<Search></Search>} />
+				</Routes>
+			</userContext.Provider>
+		</>
+	);
 }
 
-
-export default App
+export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,84 +1,67 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 import logo from './img/logo.png';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar(){
-    const [tab, setTab] = useState('');
-    const [isLogined, setLogined] = useState(localStorage.getItem('logined')); 
-    const loginCheck = async () => {
-        var logincheck = localStorage.getItem("logined")
-        setLogined(logincheck)
-        console.log(isLogined)
-    }
+function Navbar() {
+	const [tab, setTab] = useState('');
 
-    useEffect(() => {
-        loginCheck();
-        console.log("새로고침했쪄! ")
-  },)
-    return( 
-        <div className="navbar">
-            <div>
-                <Link to="/" style={{ textDecoration: 'none' }}>
-                    <div className="title">
-                        <img src={logo} alt="logo" className="logoimg"></img>
-                    </div>               
-                </Link>
-                <div className="search">
-                    <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                    <textarea className="searchArea"/>
-                </div>                
-            </div>
-            <div className="buttons">
-                <Link to="/">
-                    <button className={`navBtn ${tab === 'first' ? 'active' : ''}`}
-                        onClick={()=>setTab('first')}>홈</button>                    
-                </Link>
-            </div>
-            <div className="buttons">
-                <Link to="/calendar">
-                    <button className={`navBtn ${tab === 'second' ? 'active' : ''}`}
-                        onClick={()=>setTab('second')}>페스티벌 캘린더</button>
-                </Link>
-            </div>
-            <div className="buttons">
-                <Link to="/festlist">
-                    <button className={`navBtn ${tab === 'third' ? 'active' : ''}`}
-                        onClick={()=>setTab('third')}>페스티벌 정보</button>
-                </Link>
-            </div>
-            <div className="buttons">
-                <button className={`navBtn ${tab === 'fourth' ? 'active' : ''}`}
-                        onClick={()=>setTab('fourth')}>추천 페스티벌 테마</button>                
-            </div>
-            <div className="buttons">
-                <Link to='/review'>
-                    <button className={`navBtn ${tab === 'fifth' ? 'active' : ''}`}
-                        onClick={()=>setTab('fifth')}>커뮤니티</button>
-                </Link>
-            </div>
-            <div className="line"></div>
-            <div className="buttons">
-                {
-                    (isLogined === "true")
-                        ? (<>
-                            <Link to ="/mypage">
-                                <button className = "navBtn">마이페이지</button>
-                            </Link>
-                        </>)
-                        :
-                        <>
-                            <Link to ="/login">
-                                <button className = "navBtn">로그인</button>
-                            </Link>
-                        </>
-                }
-            </div>           
-        </div>
-
-    )
+	return (
+		<div className='navbar'>
+			<div>
+				<Link to='/' style={{ textDecoration: 'none' }}>
+					<div className='title'>
+						<img src={logo} alt='logo' className='logoimg' />
+					</div>
+				</Link>
+				<div className='navSearch'>
+					<FontAwesomeIcon icon={faMagnifyingGlass} />
+					<input className='searchArea' />
+				</div>
+			</div>
+			<div className='buttons'>
+				<Link to='/'>
+					<button className={`navBtn ${tab === 'first' ? 'active' : ''}`} onClick={() => setTab('first')}>
+						홈
+					</button>
+				</Link>
+			</div>
+			<div className='buttons'>
+				<Link to='/calendar'>
+					<button className={`navBtn ${tab === 'second' ? 'active' : ''}`} onClick={() => setTab('second')}>
+						페스티벌 캘린더
+					</button>
+				</Link>
+			</div>
+			<div className='buttons'>
+				<Link to='/festlist'>
+					<button className={`navBtn ${tab === 'third' ? 'active' : ''}`} onClick={() => setTab('third')}>
+						페스티벌 정보
+					</button>
+				</Link>
+			</div>
+			<div className='buttons'>
+				<button className={`navBtn ${tab === 'fourth' ? 'active' : ''}`} onClick={() => setTab('fourth')}>
+					추천 페스티벌 테마
+				</button>
+			</div>
+			<div className='buttons'>
+				<Link to='/review'>
+					<button className={`navBtn ${tab === 'fifth' ? 'active' : ''}`} onClick={() => setTab('fifth')}>
+						커뮤니티
+					</button>
+				</Link>
+			</div>
+			<div className='line'></div>
+			<div className='buttons'>
+				<Link to='/login'>
+					<button className='navBtn'>로그인</button>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
